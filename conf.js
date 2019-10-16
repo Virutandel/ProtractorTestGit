e0fd7c3bf39a2290d39dde7caa42977ff243f9a5
Virutandel <48075602+Virutandel@users.noreply.github.com>
2019-10-15 22:23:53 -0700

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ exports.config = {
     directConnect: true,
 
 
-   // seleniumAddress:'http://localhost:4444/wd/hub/static/resource/hub.html', 
+    seleniumAddress:'http://localhost:4444/wd/hub/static/resource/hub.html', 
     specs: ['test_spec/Wynn5.2_spec.js'],
     framework: 'jasmine2',
 
